cfb05007a63f035a155785bd4530133de26e890d
dennistuk95 <88230815+dennistuk95@users.noreply.github.com>
2021-09-22 17:30:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var paintWalls = function (color, direction) {
+    console.log("The" + " " + direction + " " + "wall has been painted" + ' ' + color);
+};
+
+// Deel 1 - without argument
+
+paintWalls();
+
+// Deel 2 - one argument
+
+paintWalls('red');
+paintWalls('green');
+
+
+// Deel 3 - multiple arguments
+
+paintWalls('orange', 'north');
+paintWalls('grey', 'south-east');
+
